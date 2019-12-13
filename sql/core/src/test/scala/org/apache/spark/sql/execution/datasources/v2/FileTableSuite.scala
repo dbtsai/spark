2fc862daf4ca0d96b1649409d13c7cd2c43ae41b
DB Tsai <d_tsai@apple.com>
2019-12-12 17:37:39 -0800

    DSV2 partitioning should not accept nested columns

--- sql/core/src/test/scala/org/apache/spark/sql/execution/datasources/v2/FileTableSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/datasources/v2/FileTableSuite.scala
@@ -20,7 +20,8 @@ import scala.collection.JavaConverters._
 
 import org.apache.hadoop.fs.FileStatus
 
-import org.apache.spark.sql.{QueryTest, SparkSession}
+import org.apache.spark.sql.{AnalysisException, QueryTest, SparkSession}
+import org.apache.spark.sql.connector.expressions.{FieldReference, IdentityTransform, Transform}
 import org.apache.spark.sql.connector.read.ScanBuilder
 import org.apache.spark.sql.connector.write.WriteBuilder
 import org.apache.spark.sql.execution.datasources.FileFormat
@@ -84,5 +85,36 @@ class FileTableSuite extends QueryTest with SharedSparkSession {
         new DummyFileTable(spark, options, Seq(pathName), expectedDataSchema, userSpecifiedSchema)
       assert(table.dataSchema == expectedDataSchema)
     }
+  }
+
+  test("SPARK-30289: DSv2 `FileTable` partitioning should not accept nested columns") {
+    val nestedSchema = StructType(Array(
+      StructField("nested", StructType(Array(
+        StructField("id", IntegerType, true),
+        StructField("data", StringType, true)
+      )), true)
+    ))
+
+    val e = intercept[AnalysisException] {
+      new FileTable(spark, CaseInsensitiveStringMap.empty(), Seq(), Some(nestedSchema)) {
+        override def partitioning: Array[Transform] =
+          Array(IdentityTransform(FieldReference(Seq("nested", "id"))))
+
+        override def inferSchema(files: Seq[FileStatus]): Option[StructType] = Some(nestedSchema)
+
+        override def name(): String = "Dummy"
+
+        override def formatName: String = "Dummy"
+
+        override def newScanBuilder(options: CaseInsensitiveStringMap): ScanBuilder = null
+
+        override def newWriteBuilder(options: CaseInsensitiveStringMap): WriteBuilder = null
+
+        override def supportsDataType(dataType: DataType): Boolean = dataType == StringType
+
+        override def fallbackFileFormat: Class[_ <: FileFormat] = classOf[TextFileFormat]
+      }
+    }
+    assert(e.getMessage.contains("Cannot partition by nested column: nested.id"))
   }
 }
